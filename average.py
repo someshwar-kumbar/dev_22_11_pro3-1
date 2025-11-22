36e22308ebc0001e28ff3e8b484a7e2e8f368ddc
someshwar-kumbar <tncxsomu@gmail.com>
2025-11-22 12:09:18 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,8 +1,8 @@
-sub1=input("enter 1 st subject marks")
-sub2=input("enter 2 st subject marks")
-sub3=input("enter 3st subject marks")
-sub4=input("enter 4st subject marks")
-sub5=input("enter 5 st subject marks")
+sub1=int(input("enter 1 st subject marks"))
+sub2=int(input("enter 2 st subject marks"))
+sub3=int(input("enter 3st subject marks"))
+sub4=int(input("enter 4st subject marks"))
+sub5=int(input("enter 5 st subject marks"))
 avg=sub1+sub2+sub3+sub4+sub5/5
 
 if(avg>80):
