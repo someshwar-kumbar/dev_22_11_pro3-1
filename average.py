82fef908f7572b78b5e580f5ba245a9f2ff9b7f3
someshwar-kumbar <tncxsomu@gmail.com>
2025-11-22 12:13:42 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,18 +1,19 @@
-sub1=int(input("enter 1 st subject marks"))
-sub2=int(input("enter 2 st subject marks"))
-sub3=int(input("enter 3st subject marks"))
-sub4=int(input("enter 4st subject marks"))
-sub5=int(input("enter 5 st subject marks"))
-avg=sub1+sub2+sub3+sub4+sub5/5
+sub1 = int(input("Enter 1st subject marks: "))
+sub2 = int(input("Enter 2nd subject marks: "))
+sub3 = int(input("Enter 3rd subject marks: "))
+sub4 = int(input("Enter 4th subject marks: "))
+sub5 = int(input("Enter 5th subject marks: "))
 
-if(avg>80):
-  print("A grade")
-elif(avg>60 or avg<80):
-  print("B grade")
-elif(avg>40 or avg<60):
-  print("C grade")
-elif(avg>30 or avg>40):
-  print("D grade")
+avg = (sub1 + sub2 + sub3 + sub4 + sub5) / 5
+print("Average =", avg)
+
+if avg >= 80:
+    print("A grade")
+elif avg >= 60:
+    print("B grade")
+elif avg >= 40:
+    print("C grade")
+elif avg >= 30:
+    print("D grade")
 else:
-  print("fail")
-
+    print("Fail")
